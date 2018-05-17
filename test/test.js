cf88e276cec2fd770343e7320e1495918cff6bde
Brian <brian.musial@gmail.com>
2018-05-17 10:33:22 -0700

    added travis

---
+++ test/test.js
@@ -0,0 +1,11 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http')
+
+
+const expect = chai.expect
+
+describe('User', function () {
+    it('should see hello on root page', function(){
+        expect(true).to.be.true
+    })
+})
